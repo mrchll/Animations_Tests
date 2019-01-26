229a3d9b92e2e84e232b29ae29f76151f72cbfa1
Soraya <sushi@MacBook-Pro-de-Soraya.local>
2019-01-25 21:40:43 -0500

    new animations

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// DIAPORAMA
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -41,4 +43,43 @@ function showSlides(n)
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' active';
   captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+// MUSIC PLAYER
+
+function myFunction()
+{
+  var x = document.getElementById('myDIV');
+  var y = document.getElementById('myDIV2');
+
+  if ((x.style.display === 'block' && y.style.display === 'block') ||
+  (x.style.display === 'block' && y.style.display === 'none'))
+  {
+    y.style.display = 'none';
+    x.style.display = 'none';
+    document.getElementById('text').innerHTML = '';
+  } else
+  {
+    y.style.display = 'block';
+    setTimeout(function ()
+    {
+      x.style.display = 'block';
+      y.style.display = 'none';
+      document.getElementById('text').innerHTML = 'Enjoy the music!';
+    }, 1000);
+  }
+}
+
+// BIRDS SHEETS
+
+function bigImg(x)
+{
+  x.style.height = '400px';
+  x.style.width = '400px';
+}
+
+function normalImg(x)
+{
+  x.style.height = '200px';
+  x.style.width = '200px';
 }
